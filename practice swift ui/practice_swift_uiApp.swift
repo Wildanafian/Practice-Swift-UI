8dc5c563fdba04482110362c4664dd2f761c729e
wildan.nafian <wildan.nafian@spesolution.com>
2024-04-30 10:06:34 +0700

    Initial Commit

---
+++ practice swift ui/practice_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_swift_uiApp.swift
+//  practice swift ui
+//
+//  Created by Wildan on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct practice_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
